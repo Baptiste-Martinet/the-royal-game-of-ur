f8e4d9e909ba9b36de91424dc0623d07cfecb543
Baptiste Martinet <baptiste.martinet@epitech.eu>
2020-12-23 20:22:58 +0100

    Added color theme & minor fixes

--- client.js
+++ client.js
@@ -24,6 +24,21 @@ class Cell {
   }
 }
 
+class ColorTheme {
+  constructor (_background, _grid, _cells, _diceTriangles, _diceCircles, _hoveredCells, _pieceWhite, _pieceBlack, _cellCornerRadius, _diceTriangleStorkes, _diceTriangleStrokesSize) {
+    this.background = _background;
+    this.grid = _grid;
+    this.cells = _cells;
+    this.diceTriangles = _diceTriangles;
+    this.diceCircles = _diceCircles;
+    this.hoveredCells = _hoveredCells;
+    this.pieceColors = [_pieceWhite, _pieceBlack];
+    this.cellCornerRadius = _cellCornerRadius;
+    this.diceTriangleStrokes = _diceTriangleStorkes;
+    this.diceTriangleStrokesSize = _diceTriangleStrokesSize;
+  }
+}
+
 const CELL_SIZE = 80;
 const WHITE = 0;
 const BLACK = 1;
@@ -38,10 +53,12 @@ const MOVING = 'moving';
 var whosturn = -1;
 var players = [new Player(0), new Player(1)];
 var board = new Array(14);
+var theme = null;
 
 /* game variables */
 var topLeft = null;
-var diceValue = -1;
+var diceValues = [0, 1, 0, 0];
+var totalDicesValue = -1;
 var playerState = WAITING;
 
 /* UI varaibles */
@@ -49,12 +66,22 @@ var button = null;
 
 /* utils functions */
 
+function drawPiece(pos, color)
+{
+  let p5PieceColor = theme.pieceColors[color];
+
+  stroke(red(p5PieceColor), green(p5PieceColor), green(p5PieceColor), 180);
+  strokeWeight(6);
+  fill(p5PieceColor);
+  circle(pos.x + CELL_SIZE / 2, pos.y + CELL_SIZE / 2, CELL_SIZE * 0.7);
+}
+
 function drawCell(cell)
 {
-  stroke(0);
+  stroke(theme.grid);
   strokeWeight(2);
-  fill(255);
-  square(cell.pos.x, cell.pos.y, CELL_SIZE);
+  fill(theme.cells);
+  square(cell.pos.x, cell.pos.y, CELL_SIZE, theme.cellCornerRadius);
 }
 
 function displayBoard()
@@ -65,28 +92,37 @@ function displayBoard()
   }
 }
 
+function displayPieces()
+{
+  for (let i = 0; i < 14; ++i) {
+    if (board[i][WHITE].isOccupied)
+      drawPiece(board[i][WHITE].pos, WHITE);
+    if (board[i][BLACK].isOccupied)
+      drawPiece(board[i][BLACK].pos, BLACK);
+  }
+}
+
 function displayHoveredCells()
 {
   for (let i = 0; i < 14; ++i) {
     if (board[i][players[ME].color].isHovered) {
-      fill(255, 0, 0);
-      square(board[i][players[ME].color].pos.x, board[i][players[ME].color].pos.y, CELL_SIZE);
+      noStroke();
+      fill(theme.hoveredCells);
+      square(board[i][players[ME].color].pos.x, board[i][players[ME].color].pos.y, CELL_SIZE, theme.cellCornerRadius);
     }
   }
 }
 
 function drawNbPieces(player)
 {
-  let pos = new Vec2d(topLeft.x + (CELL_SIZE * 4.5), topLeft.y + (CELL_SIZE * (player.color == WHITE ? 2.5 : 0.5)));
+  let pos = new Vec2d(topLeft.x + (CELL_SIZE * 4), topLeft.y + (CELL_SIZE * (player.color == WHITE ? 2 : 0)));
 
-  noStroke();
-  fill(0, 0, 255);
-  ellipse(pos.x, pos.y, CELL_SIZE * 0.8);
+  drawPiece(pos, player.color);
   fill(255);
   noStroke();
   textSize(32);
   textAlign(CENTER, CENTER);
-  text('7', pos.x, pos.y);
+  text('7', pos.x + CELL_SIZE / 2, pos.y + CELL_SIZE / 2);
 }
 
 function displayNbPieces()
@@ -97,7 +133,9 @@ function displayNbPieces()
 
 function drawTriangle(bottomLeftCoords, size)
 {
-  fill(245, 153, 49);
+  stroke(theme.diceTriangleStrokes);
+  strokeWeight(theme.diceTriangleStrokesSize);
+  fill(theme.diceTriangles);
   triangle(bottomLeftCoords.x, bottomLeftCoords.y, bottomLeftCoords.x + (size / 2), bottomLeftCoords.y - (size * 0.9), bottomLeftCoords.x + size, bottomLeftCoords.y);
 }
 
@@ -108,6 +146,12 @@ function displayDicesTriangles()
 
   for (let i = 0; i < 4; ++i) {
     drawTriangle(bottomLeft, triangleSize);
+
+    if (diceValues[i] == 1) {
+      fill(theme.diceCircles);
+      noStroke();
+      circle(bottomLeft.x + triangleSize / 2, bottomLeft.y - triangleSize * 0.35, triangleSize * 0.3);
+    }
     bottomLeft.x += triangleSize + 6;
   }
 }
@@ -164,11 +208,28 @@ function setup() {
   button.mousePressed(buttonPressed);
   button.hide();
 
+  theme = new ColorTheme(
+    color(17, 17, 17), /* background color */
+    color(156, 43, 255), /* grid color */
+    color(0 , 0), /* cells color */
+    color(0, 0), /* dice triangles color */
+    color(255), /* dice circles color */
+    color(70, 100), /* hovered cell color */
+    color(201, 0, 0), /* white piece color */
+    color(0, 182, 201), /*black piece color */
+    15, /* cell corner radius */
+    color(255), /* dice triangle stroke color */
+    2, /* dice triangle stroke size */
+  );
+
   for (let i = 0; i < 14; ++i) {
     board[i] = [new Cell(new Vec2d(0, 0)), new Cell(new Vec2d(0, 0))]; //white cell & black cell
   }
   setCellsPos(); //setting up the tiles positions for black and white
 
+  board[0][0].isOccupied = true;
+  board[6][0].isOccupied = true;
+  board[7][1].isOccupied = true;
   /* socket */
   setPlayerState(DRAWING_DICE);
 }
@@ -182,8 +243,9 @@ function draw() {
   /*COMPUTE */
   
   /* DRAW */
-  background(240);
+  background(theme.background);
   displayBoard();
+  displayPieces();
   displayHoveredCells();
   displayNbPieces();
   displayDicesTriangles();
