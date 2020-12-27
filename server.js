b400a996eb56f4a27ad57e1f1d5c115e1ff8c2c3
Baptiste Martinet <baptiste.marti@outlook.com>
2020-12-27 23:11:45 +0100

    Started implementing server functions

--- server.js
+++ server.js
@@ -12,12 +12,54 @@ var socket = require('socket.io');
 
 var io = socket(server);
 
+const { v4: uuidv4 } = require('uuid');
+
 /* server code */
+
+class Room {
+    constructor(_id) {
+        this.id = _id;
+    }
+}
+
+var rooms = [];
+
+function getRoomById(id)
+{
+    let len = rooms.length;
+
+    for (let i = 0; i < len; ++i) {
+        if (rooms[i].id == id)
+            return rooms[i];
+    }
+    return null;
+}
 
 io.sockets.on('connection', (socket) => {
     console.log('new user:', socket.id);
 
+    var myRoom = undefined;
+
+    socket.on('createRoom', () => {
+        let newRoom = new Room(uuidv4()); //creer la room
+        rooms.push(newRoom); //la stocker dans un array
+        socket.emit('joinedRoom', myRoom); //envoyer l'event au client qu'il peu rejoindre la room
+    });
+
+    socket.on('connectToRoom', (id) => {
+        if (!(myRoom = getRoomById(id))) {
+            console.log('User tried to connect to non existent room', id);
+            return;
+        }
+        socket.join(myRoom.id);
+    });
+
     socket.on('disconnect', () => {
         console.log('User disconnected.', socket.id);
+
+        if (myRoom === undefined) {
+            return;
+        }
+        /* TODO room interaction */
     });
 });
